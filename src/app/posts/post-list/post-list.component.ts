74b5cc3f730bb86ad849cbae22abc944a5a490f2
RashidNaveed <chrashidnaveed95@gmail.com>
2019-12-24 02:40:31 +0500

    Setting up backend

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription } from "rxjs";
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+posts: Post[] = [];
+private postsSub: Subscription;
+// posts = [
+//   {title:'First Post', content:'This is the first post\'s content'},
+//   {title:'Second Post', content:'This is the Second post\'s content'},
+//   {title:'Third Post', content:'This is the Third post\'s content'}
+// ];
+  constructor(public postsService: PostsService) { }
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+   this.postsSub = this.postsService.getPostUpdateListener()
+     .subscribe( (posts:Post[]) => {
+      this.posts = posts;
+    });
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
