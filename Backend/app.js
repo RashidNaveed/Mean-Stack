843ebac0908f25991d894d68d891aa72cf2a5f06
RashidNaveed <chrashidnaveed95@gmail.com>
2019-12-25 01:31:43 +0500

    making restful api and defining the /path to fetch data from backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res , next) => {
-  console.log('first middleware');
-next();
-});
 
-app.use((req, res , next) => {
+app.use('/api/posts', (req, res , next) => {
 res.send('welcome to express!');
 });
 
